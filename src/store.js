574771cc8518a5185c144ed05df9d14655cc3034
teffcode <tefy.1@live.com>
2017-10-23 14:25:22 -0500

    💚 Combine Reducers

--- src/store.js
+++ src/store.js
@@ -1,28 +1,48 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk'; // middleware
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk'; // middleware
 
-// función reductora
-const reducer = (state, action) => {
+
+// esta es una porción del reducer, que recibe un pedacito del estado
+// state=[] quiere decir que si es undefined retorna el estado inicial que es state=[]
+const products = (state=[], action) => {
     if(action.type === 'REPLACE_PRODUCTS'){
-        return {
-            ...state,
-            products: action.products // lo que nos llegue de la acción
-        };
-    }
-    else if(action.type === 'ADD_TO_CART'){
-        return {
-            ...state,
-            cart: state.cart.concat(action.product)
-        };
-    }
-    else if(action.type === 'REMOVE_FROM_CART'){
-        return {
-            ...state,
-            cart: state.cart.filter(product => product.id !== action.product.id )
-        };
+        return action.products
     }
     return state;
 }
+
+const cart = (state=[], action) => {
+    if(action.type === 'ADD_TO_CART'){
+        return state.concat(action.product)
+    }
+    else if(action.type === 'REMOVE_FROM_CART'){
+        return state.filter(product => product.id !== action.product.id )
+    }
+    return state;
+}
+
+// FUNCIÓN REDUCTORA SIN COMBINAR 
+// const reducer = (state, action) => {
+//     if(action.type === 'REPLACE_PRODUCTS'){
+//         return {
+//             ...state,
+//             products: action.products // lo que nos llegue de la acción
+//         };
+//     }
+//     else if(action.type === 'ADD_TO_CART'){
+//         return {
+//             ...state,
+//             cart: state.cart.concat(action.product)
+//         };
+//     }
+//     else if(action.type === 'REMOVE_FROM_CART'){
+//         return {
+//             ...state,
+//             cart: state.cart.filter(product => product.id !== action.product.id )
+//         };
+//     }
+//     return state;
+// }
 
 // Middleware
 // http://redux.js.org/docs/advanced/Middleware.html
@@ -37,4 +57,5 @@ const logger = store => next => action => {
 // 1. la función reductora
 // 2. el estado inicial
 // Opcional -> Middleware
-export default createStore(reducer, { cart: [], products: [] }, applyMiddleware(logger, thunk));
+// export default createStore(reducer, { cart: [], products: [] }, applyMiddleware(logger, thunk));
+export default createStore(combineReducers({ cart, products }), applyMiddleware(logger, thunk));
